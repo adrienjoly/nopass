e63eaa89881945628d491454830820b79e13dc94
Adrien Joly <adrien@whyd.com>
2017-06-26 19:45:28 +0200

    jasmine tests for db-memory.js

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ var connect = require('connect');
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var db = require('./db-memory');
-/* db tests:
-db.fetchData()
-db.fetchData('/test')
-db.setData('/test', 'coucou');
-db.fetchData('/test')
-db.fetchData()
-*/
 
 var PORT = process.env.PORT || 8080;
 
---
+++ test.js
@@ -0,0 +1,36 @@
+var assert = require('assert')
+var db = require('./db-memory')
+
+const TEST_VALUE = 'coucou'
+
+describe('db-memory', function() {
+
+  it('returns empty on initial fetch', function(done) {
+    db.fetchData('/', ({ key, value }) =>
+      done(expect(JSON.stringify(value)).toBe('{}')))
+  })
+
+  it('returns undefined when fetching /test', function(done) {
+    db.fetchData('/test', ({ key, value }) =>
+      done(expect(value).toBe(undefined)))
+  })
+
+  it('returns value at /test after setting it', function(done) {
+    db.setData('/test', TEST_VALUE, function() {
+      db.fetchData('/test', ({ key, value }) =>
+        done(expect(value).toBe(TEST_VALUE)))
+    })
+  })
+
+  it('returns { test: value } at / after setting it', function(done) {
+    db.setData('/test', TEST_VALUE, function() {
+      db.fetchData('/', ({ key, value }) =>
+        done(
+          expect(JSON.stringify(value))
+            .toBe(JSON.stringify({ test: TEST_VALUE }))
+        )
+      )
+    })
+  })
+
+})
