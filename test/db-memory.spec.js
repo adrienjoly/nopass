88dfa4f9a70831bb25da748fcca346f906fb9730
Adrien Joly <adrien@whyd.com>
2017-06-26 20:24:17 +0200

    db-memory: pushData()

--- test/db-memory.spec.js
+++ test/db-memory.spec.js
@@ -2,6 +2,9 @@ var db = require('../lib/db-memory')
 
 const TEST_VALUE = 'coucou'
 
+Object.prototype.values = Object.prototype.values || ((obj) =>
+  Object.keys(obj).map(field => obj[field]));
+
 describe('db-memory', function() {
 
   it('returns empty on initial fetch', function(done) {
@@ -30,6 +33,30 @@ describe('db-memory', function() {
         )
       )
     })
+  })
+
+  /*
+  it('returns { test2: { test: value } } after setting /test2/test', function(done) {
+    db.setData('/test2/test', TEST_VALUE, function() {
+      db.fetchData('/', ({ key, value }) =>
+        done(
+          expect(JSON.stringify(value))
+            .toBe(JSON.stringify({ test2: { test: TEST_VALUE }}))
+        )
+      )
+    })
+  })
+  */
+
+  it('returns { (uuid) : value } after pushing to /testpush', function(done) {
+    db.pushData('/testpush', TEST_VALUE, function() {
+      db.fetchData('/testpush', ({ key, value }) =>
+        done(
+          expect(Object.values(value).join(''))
+            .toBe(TEST_VALUE)
+        )
+      )
+    })    
   })
 
 })
